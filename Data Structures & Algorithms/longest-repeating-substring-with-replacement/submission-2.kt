7c2f1406144c8609273639d1acd843535921adce
baltekg <bartlomiej.gajda2@gmail.com>
2026-05-03 18:31:04 +0200

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+            var startIndex = 0
+            var mostPopularCharCount = 0
+            val freq = mutableMapOf<Char, Int>()
+            var result = 0
+
+            for(i in 0 until s.length) {
+                freq[s[i]] = (freq[s[i]] ?: 0) + 1
+                mostPopularCharCount = max(mostPopularCharCount, freq[s[i]]!!)
+
+                while(i-startIndex+1-mostPopularCharCount > k) {
+                    freq[s[startIndex]] = freq[s[startIndex]]!! - 1
+                    startIndex++
+                }
+
+                result = max(result, i-startIndex+1)
+            }
+        return result
+    }
+}
