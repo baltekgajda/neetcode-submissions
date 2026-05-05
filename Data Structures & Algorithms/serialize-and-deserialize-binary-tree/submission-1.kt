5de1592fc094c6927c3491393e90abbf533644d5
baltekg <bartlomiej.gajda2@gmail.com>
2026-05-05 11:38:07 +0200

    Add: serialize-and-deserialize-binary-tree - submission-1

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-1.kt
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Codec {
+    // Encodes a tree to a single string.
+    fun serialize(root: TreeNode?): String {
+        val result = mutableListOf<String>()
+        if(root == null) {
+            return "N"
+        }
+
+        
+        return root.`val`.toString() + "#" + serialize(root?.left) + serialize(root?.right)
+    }
+
+    // Decodes your encoded data to tree.
+    fun deserialize(data: String): TreeNode? {
+
+        //returns first of first unused char
+        fun deserialize(startIndex: Int): Pair<TreeNode?, Int> {
+            if(data[startIndex] == 'N') {
+                return null to startIndex+1
+            }
+
+            var index = startIndex
+            while(data[index] != '#') {
+                index++
+            }
+            
+            val value = data.substring(startIndex, index).toInt()
+            val node = TreeNode(value)
+            val (left, nextIndex) = deserialize(index+1)
+            val (right, returnIndex) = deserialize(nextIndex)
+            node.left = left
+            node.right = right
+            return node to returnIndex
+        }
+
+        return deserialize(0).first
+    }
+}
