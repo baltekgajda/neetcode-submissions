ed79d26de2f9bcfcd98ac76ca2600d2cb3c95e42
baltekg <bartlomiej.gajda2@gmail.com>
2026-06-08 11:01:11 +0200

    Add: delete-node-in-a-bst - submission-2

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-2.kt
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun deleteNode(root: TreeNode?, key: Int): TreeNode? {
+        val stubRoot = TreeNode(Int.MIN_VALUE)
+        stubRoot.left = root
+
+        val (node, parent) = findNode(root, stubRoot, key) ?: return root
+        fun assignNewNode(newValue: TreeNode?) {
+            if(parent.left == node) {
+                parent.left = newValue
+            } else {
+                parent.right = newValue
+            }
+        }
+        
+        //no children
+        if(node.left == null && node.right == null) {
+            assignNewNode(null)
+            return stubRoot.left
+        }
+
+        //one child only
+        if(node.left == null) {
+            assignNewNode(node.right)
+            return stubRoot.left
+        }
+
+        //one child only
+        if(node.right == null) {
+            assignNewNode(node.left)
+            return stubRoot.left
+        }
+
+        //two children
+        var prev: TreeNode? = node
+        var next: TreeNode? = node.right
+        while(next?.left != null) {
+            prev = next
+            next = next?.left
+        }
+
+        node.`val` = next?.`val`!!
+        if (prev == node) {
+            prev.right = next?.right
+        } else {
+            prev?.left = next?.right
+        }
+
+        return stubRoot.left
+    }
+
+    fun findNode(node: TreeNode?, parent: TreeNode,  key: Int): Pair<TreeNode, TreeNode>? {
+        node ?: return null
+
+        if(node.`val` == key) {
+            return node to parent
+        }
+
+        val left = findNode(node?.left, node!!, key)
+        return if(left != null) {
+            left
+        } else {
+            findNode(node?.right, node!!, key)
+        }
+    }
+}
